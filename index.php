ce31eb2763ff1c3e248eb7271cb0210619a81c76
SAretz <sebastian.aretz@alumni.fh-aachen.de>
2017-05-03 12:49:54 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$site = "home";
+
+if(isset($_GET["page"]))
+{
+    $site = $_GET["page"];
+}
+
+require_once "header.php";
+
+require_once "views/".$site.".php";
+
+require_once "footer.php";
+
---
+++ views/home.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sebastian
+ * Date: 03.05.2017
+ * Time: 12:36
+ */
