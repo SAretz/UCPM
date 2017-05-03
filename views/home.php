a5326266a3a8eca73c0c1b13a1b097f8a8a36175
Marius Hartmann <marius8hartmann@gmail.com>
2017-05-03 14:19:20 +0200

    Rank

--- views/home.php
+++ views/home.php
@@ -6,13 +6,4 @@ require_once __DIR__."/../Model/feedback.php";
 $db = new Database();
 $result = $db->Select(new Feedback());
 
-//var_dump($result);
-
-$model = new Feedback();
-$model->Bewertung = 5;
-$model->Kurztext = "Mal schauen";
-$model->Projekt = "1";
-
-$result = $db->Insert($model);
-
 var_dump($result);
